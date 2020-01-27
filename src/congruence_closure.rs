704c2832965212454e89fe1ca8b1ba177c93d04f
Daniël Louwrink <daniel.louwrink@gmail.com>
2020-01-27 14:51:10 +0100

    explanation generation

--- src/congruence_closure.rs
+++ src/congruence_closure.rs
@@ -1,5 +1,5 @@
 use std::collections::hash_map::Entry;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::mem;
 
 // This implementation is based on "Congruence Closure with Integer Offsets" by
@@ -37,12 +37,13 @@ fn currify(t: &Term) -> Curried {
 }
 
 type ConstMap<T> = Vec<T>;
+type ProofForest = ConstMap<Option<(Const, PendingEq)>>;
 
 struct ConstSupply {
     representatives: ConstMap<Const>,
     members: ConstMap<Vec<Const>>,
     uses: ConstMap<Vec<AppEq>>,
-    proof_parents: ConstMap<Option<(Const, PendingEq)>>,
+    proof_parents: ProofForest,
 }
 
 impl ConstSupply {
@@ -170,7 +171,58 @@ impl EqualitySolver {
         }
     }
 
-    fn add_proof_edge(&mut self, reason: PendingEq) {}
+    fn add_proof_edge(&mut self, reason: PendingEq) {
+        let (a, b) = reason.constants();
+
+        let (a, b) = if self.class_size(a) > self.class_size(b) {
+            (b, a)
+        } else {
+            (a, b)
+        };
+
+        // Reverse the path from a to the root of its tree and add edge from a
+        // to b. By doing this, we ensure that we can always find the shortest
+        // path between any two nodes in our tree by just taking those edges
+        // that go toward their common ancestor.
+        let mut previous = b;
+        let mut maybe_current = Some((a, reason));
+        while let Some((current, previous_reason)) = maybe_current {
+            let old_parent = self.const_supply.proof_parents[current.0];
+            self.const_supply.proof_parents[current.0] = Some((previous, previous_reason));
+            previous = current;
+            maybe_current = old_parent;
+        }
+    }
+
+    fn union(&mut self, a: Const, b: Const) {
+        // Make sure that the size of a's equivalence class is less than or
+        // equal to b, which is necessary to guarantee good time complexity.
+        // This is common in union-find data structures.
+        let (a, b) = if self.class_size(a) > self.class_size(b) {
+            (b, a)
+        } else {
+            (a, b)
+        };
+
+        // Merge a into b's class
+        let members_a = mem::replace(&mut self.const_supply.members[a.0], Vec::new());
+        for &m in &members_a {
+            self.reparent(m, b);
+        }
+
+        // Generate new equations according to the lookup table, which are
+        // caused by congruences.
+        let uses_a = mem::replace(&mut self.const_supply.uses[a.0], Vec::new());
+        for &eq in &uses_a {
+            let AppEq(app, _) = eq;
+            if let Some(lookupeq) = self.lookup(app) {
+                self.pending.push(PendingEq::Application(eq, lookupeq));
+            } else {
+                self.lookup_set(app, eq);
+                self.const_supply.uses[b.0].push(eq);
+            }
+        }
+    }
 
     pub fn propagate(&mut self) {
         while let Some(eq) = self.pending.pop() {
@@ -188,33 +240,124 @@ impl EqualitySolver {
             // explanations.
             self.add_proof_edge(eq);
 
-            // Make sure that the size of a's equivalence class is less than or
-            // equal to b, which is necessary to guarantee good time complexity.
-            // This is common in union-find data structures.
-            let (a, b) = if self.class_size(a) > self.class_size(b) {
-                (b, a)
-            } else {
-                (a, b)
-            };
+            self.union(a, b);
+        }
+    }
+}
 
-            // Merge a into b's class
-            let members_a = mem::replace(&mut self.const_supply.members[a.0], Vec::new());
-            for &m in &members_a {
-                self.reparent(m, b);
-            }
+pub struct ExplanationGenerator<'a> {
+    proof_forest: &'a ProofForest,
+    // For each represenatative, the highest node in its class. A node is higher
+    // if it is closer to the root of its proof tree in the proof forest. During
+    // explanation generation, we can typically jump to the highest node in a
+    // class, thereby skipping many edges of the proof tree.
+    highest_node: ConstMap<Option<Const>>,
+    // Union-find data structure.
+    // This only keeps track of elements which are equal
+    // due to the PROOF OUTPUT SO FAR (i.e., things might be non-equal which are
+    // in fact equal in the original EqualitySolver).
+    representatives: ConstMap<Const>,
+    // For each representative, the list of its members
+    members: ConstMap<Vec<Const>>,
+}
 
-            // Generate new equations according to the lookup table, which are
-            // caused by congruences.
-            let uses_a = mem::replace(&mut self.const_supply.uses[a.0], Vec::new());
-            for &eq in &uses_a {
-                let AppEq(app, _) = eq;
-                if let Some(lookupeq) = self.lookup(app) {
-                    self.pending.push(PendingEq::Application(eq, lookupeq));
-                } else {
-                    self.lookup_set(app, eq);
-                    self.const_supply.uses[b.0].push(eq);
-                }
+impl<'a> ExplanationGenerator<'a> {
+    fn new(proof_forest: &'a ProofForest) -> Self {
+        let num_constants = proof_forest.len();
+
+        let mut highest_node = Vec::with_capacity(num_constants);
+        let mut representatives = Vec::with_capacity(num_constants);
+        let mut members = Vec::with_capacity(num_constants);
+        // Initially, each node is its own representative, and also the highest
+        // node in its own class.
+        for i in 0..num_constants {
+            let new_const = Const(i);
+            highest_node.push(Some(new_const));
+            representatives.push(new_const);
+            members.push(vec![new_const]);
+        }
+
+        ExplanationGenerator {
+            proof_forest,
+            highest_node,
+            representatives,
+            members,
+        }
+    }
+
+    fn representative(&self, c: Const) -> Const {
+        self.representatives[c.0]
+    }
+
+    fn parent(&self, c: Const) -> Option<(Const, PendingEq)> {
+        self.proof_forest[c.0]
+    }
+
+    fn class_size(&self, c: Const) -> usize {
+        let c = self.representative(c);
+        self.members[c.0].len()
+    }
+
+    fn highest_node(&self, c: Const) -> Const {
+        let c = self.representative(c);
+        self.highest_node[c.0]
+            .expect("Representative should keep track of the highest node in its class")
+    }
+
+    fn collect_ancestors(&self, c: Const) -> Vec<Const> {
+        let mut ancestors = Vec::new();
+
+        let mut maybe_current = Some(c);
+        while let Some(current) = maybe_current {
+            ancestors.push(current);
+            maybe_current = self.parent(current).map(|x| x.0);
+        }
+
+        ancestors
+    }
+
+    fn nearest_common_ancestor(&self, c1: Const, c2: Const) -> Option<Const> {
+        let ancestors_c1 = self.collect_ancestors(c1);
+
+        let mut ancestors_c2 = vec![false; self.proof_forest.len()];
+        for a2 in self.collect_ancestors(c2) {
+            ancestors_c2[a2.0] = true;
+        }
+
+        for a1 in ancestors_c1 {
+            if ancestors_c2[a1.0] {
+                return Some(a1);
             }
         }
+
+        return None;
+    }
+
+    fn union_classes(&mut self, a: Const, b: Const, highest_node: Const) {
+        let a = self.representative(a);
+        let b = self.representative(b);
+
+        let (a, b) = if self.class_size(a) > self.class_size(b) {
+            (b, a)
+        } else {
+            (a, b)
+        };
+
+        // Move members from a to b
+        let members_a = mem::replace(&mut self.members[a.0], Vec::new());
+        for &m in &members_a {
+            self.representatives[m.0] = b;
+            self.members[b.0].push(m);
+        }
+
+        self.highest_node[a.0] = None;
+        self.highest_node[b.0] = Some(highest_node);
+    }
+
+    fn union_with_parent(&mut self, child: Const) {
+        let (parent, _) = self.parent(child).unwrap();
+        let highest_of_parent = self.highest_node(parent);
+
+        self.union_classes(child, parent, highest_of_parent);
     }
 }
